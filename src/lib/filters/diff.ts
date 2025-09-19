f053af50683d6fc5ee255b719e9fd1db233fbe46
Brandon-Cameron <brandoncameron26@gmail.com>
2025-09-19 13:01:20 +0100

    update

--- src/lib/filters/diff.ts
+++ src/lib/filters/diff.ts
@@ -1,7 +1,7 @@
-var previous: number = 0;
+let previous: number = 0;
 
 export function diff (current: number): number {
-    var result: number = Math.abs(current - previous)
+    let result: number = Math.abs(current - previous)
 
     previous = current;
 
