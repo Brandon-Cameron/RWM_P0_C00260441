798f1d0dd487dcca37ca9ab50d9448b01b682f2a
Brandon-Cameron <brandoncameron26@gmail.com>
2026-09-21 12:14:33 +0100

    Sprint 3: Step 2 Started

---
+++ Sprint_6/src/lib/checklistItem.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let id; // callback prop, not a dispatched event
+    export let label;
+    export let done = false;
+    export let onToggle;
+
+    function handleChange() {
+        console.log("test")
+    }
+    
+</script>
+
+
+<label>
+ <input
+   type="checkbox"
+   bind:checked={done}
+   on:change={handleChange}
+   data-testid="checkbox-{id}"
+ />
+ {label}
+</label>
